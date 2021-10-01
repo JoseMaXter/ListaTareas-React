ce2964f5f2a9f435e9f3471be14eacc75933db70
Jose_MaXter <jose162m@gmail.com>
2021-10-01 06:24:57 +0200

    agrgación del label a los inputs

--- src/Componentes/Formulario.jsx
+++ src/Componentes/Formulario.jsx
@@ -7,6 +7,7 @@ import Typography from '@mui/material/Typography'
 import { List, ListItem, ListItemText, ListItemIcon, IconButton } from '@mui/material'
 import { useState } from 'react'
 import { useForm, Controller } from 'react-hook-form'
+import FormLabel from '@mui/material/FormLabel';
 
 const Formulario = () => {
 
@@ -30,23 +31,29 @@ const Formulario = () => {
             <form action="#" onSubmit={handleSubmit(onSubmit)}>
                 <h2 className="text-center text-xl font-bold m-4 bg-blue-600 text-white p-2">Lista de tareas</h2>
                 <div className="flex flex-col p-4 ">
-                    <Controller
-                        name="nombreTarea"
-                        control={control}
-                        rules={{ required: true }}
-                        render={({ field }) => <TextField color={errors.nombreTarea ? 'error' : 'primary'} label="Nombre de la tarea" sx={{marginBottom: '1rem'}} {...field} />}
-                    />
+                    <FormLabel sx={{display: 'flex', flexDirection: 'column'}} error={errors.nombreTarea?.type === 'required' ? true : false}>
+                        <Typography variant="subtitle1" sx={{paddingBottom: '15px'}}>Nombre Tarea</Typography>
+                        <Controller
+                            name="nombreTarea"
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) => <TextField color={errors.nombreTarea ? 'error' : 'primary'} label="Nombre de la tarea" sx={{marginBottom: '1rem'}} {...field} />}
+                        />
+                    </FormLabel>
+         
+                    {errors.nombreTarea?.type === 'required' && <h4 className="p-3 text-lg text-white bg-red-600 rounded mb-4">El campo de nombre es obligatorio</h4>}
 
-                    {errors.nombreTarea?.type === 'required' && <h4 className="p-3 bg-red-700 text-white text-center rounded mb-4">El campo de NOMBRE es obligatorio</h4>}
+                    <FormLabel sx={{display: 'flex', flexDirection: 'column'}} error={errors.descripcion?.type === 'required' ? true : false}>
+                        <Typography variant="subtitle1" sx={{padding: '15px 0'}}>Descripcion</Typography>
+                        <Controller
+                            name="descripcion"
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) => <TextField color={errors.descripcion ? 'error' : 'primary'} label="Descripción" {...field} />}
+                        />
+                    </FormLabel>
 
-                    <Controller
-                        name="descripcion"
-                        control={control}
-                        rules={{ required: true }}
-                        render={({ field }) => <TextField color={errors.descripcion ? 'error' : 'primary'} label="Descripción" {...field} />}
-                    />
-
-                    {errors.descripcion?.type === 'required' && <h4 className="p-3 bg-red-700 text-white text-center rounded mt-4">El campo DESCRIPCION es obligatorio</h4>}
+                    {errors.descripcion?.type === 'required' && <h4 className="p-3 bg-red-600 text-lg text-white rounded mt-4">El campo descripción es obligatorio</h4>}
 
                     <Button variant="contained" sx={{marginTop: '1rem'}} endIcon={<AddTaskIcon />} type="submit">
                         <Typography variant="subtitle1" >
